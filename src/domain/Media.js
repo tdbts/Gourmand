c83fd3142feb510d9966b037ae2e3864b2507f2f
root <root@Lloyd.localdomain>
2020-11-23 13:32:22 -0500

    Create 'Restaurant' and 'Media' classes.

---
+++ src/domain/Media.js
@@ -0,0 +1,10 @@
+module.exports = class Media {
+
+	constructor(id, type, caption, sources) {
+		this.id = id;
+		this.type = type;
+		this.caption = caption;
+		this.sources = sources;
+	}
+
+}
---
+++ src/domain/Restaurant.js
@@ -0,0 +1,13 @@
+module.exports = class Restaurant {
+
+	constructor(id, name, address, neighborhoods, categories, mediaCount, media) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.neighborhoods = neighborhoods;
+		this.categories = categories;
+		this.mediaCount = mediaCount;
+		this.media = media;
+	}
+
+}
