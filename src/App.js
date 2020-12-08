6972514eaf31a19cbbd4594b3f3fd9b4e3f1531b
root <root@Lloyd.localdomain>
2020-12-07 22:58:10 -0500

    [ Gallery ] Accept restaurants, not just media and shuffle within component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import _  from 'underscore';
 import SearchForm from './components/SearchForm';
 import Gallery from './components/Gallery';
-import YelpMedia from './scrapers/yelp/YelpMedia';
 
 function updateSearchURL(description, location, setURL) {
 	console.log("updateSearchURL()");
@@ -22,19 +20,9 @@ function createSearchURL(description, location) {
 	return `/search?description=${encodeURIComponent(description)}&location=${encodeURIComponent(location)}`;
 }
 
-function getShuffledMedia(restaurants) {
-	console.log("restaurants:", restaurants);
-	const allMedia = restaurants.flatMap(restaurant => restaurant.media);
-	console.log("allMedia:", allMedia);
-	const shuffledMedia = _.shuffle(allMedia);
-	// console.log("shuffledMedia:", shuffledMedia);
-	return shuffledMedia.map(media => (new YelpMedia().populateFromBSON(media)));
-}
-
 function App() {
 	const [url, setURL] = useState('');
 	const [restaurants, setRestaurants] = useState([]);
-	const [shuffledMedia, setShuffledMedia] = useState([]);
 
 	useEffect(() => {
 		if (url) {
@@ -45,15 +33,11 @@ function App() {
 		}
 	}, [url])
 
-	useEffect(() => {
-		setShuffledMedia(getShuffledMedia(restaurants));
-	}, [restaurants]);
-
 	return (
 		<div className="App">
 			<h1 className="title-header">Gourmand</h1>
 			<SearchForm onSearchRequest={(description, location) => updateSearchURL(description, location, setURL)} />
-			<Gallery media={shuffledMedia} />
+			<Gallery restaurants={restaurants} />
 		</div>
 	);
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,29 @@
-const Gallery = ({media}) => {
-	console.log("media:", media);
+import { useState, useEffect } from 'react';
+import _ from 'underscore';
+import YelpMedia from '../scrapers/yelp/YelpMedia';
+
+const Gallery = ({restaurants}) => {
+	// console.log("media:", media);
+	const [shuffledMedia, setShuffledMedia] = useState([]);
+
+	useEffect(() => {
+		setShuffledMedia(getShuffledMedia(restaurants));
+	}, [restaurants]);
+
 	return (
 		<>
-			{media.map((m, i) => <img className="food-media" src={m.getThumbnailURLs()[0]} key={i} />)}
+			{shuffledMedia.map((m, i) => <img className="food-media" src={m.getThumbnailURLs()[0]} key={i} />)}
 		</>
 	);
 };
+
+function getShuffledMedia(restaurants) {
+	console.log("restaurants:", restaurants);
+	const allMedia = restaurants.flatMap(restaurant => restaurant.media);
+	console.log("allMedia:", allMedia);
+	const shuffledMedia = _.shuffle(allMedia);
+	// console.log("shuffledMedia:", shuffledMedia);
+	return shuffledMedia.map(media => (new YelpMedia().populateFromBSON(media)));
+}
 
 export default Gallery;
