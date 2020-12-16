6729aeaeb9a3937c5ee8ba7ffdecc4ea1b02c90d
root <root@Lloyd.localdomain>
2020-12-16 06:25:40 -0500

    Update research.

---
+++ .research/yelp/biz/curl.sh
@@ -0,0 +1,14 @@
+curl 'https://www.yelp.com/biz/ba-xuyen-brooklyn-2' \
+  -H 'authority: www.yelp.com' \
+  -H 'cache-control: max-age=0' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'sec-fetch-site: none' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'sec-fetch-dest: document' \
+  -H 'referer: https://www.yelp.com/search?find_desc=&find_loc=Brooklyn%2C+NY+11219&ns=1' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H 'cookie: yuv=RMN-3NTG2QzBiWlDLMyixY-BB-u9N59_59cWbD3YfOJaWJs-EzhuT9ksmA3nvsV0sgv2zlRAAzZmSgg0rBvZi7Y5HyidytUo; hl=en_US; dm=%7B%22rfb%22%3Atrue%7D; wdi=1|66283D4B28A13E96|0x1.4ef17e874677bp+30|0ead417b4e4f7aeb; _ga=GA1.2.66283D4B28A13E96; G_ENABLED_IDPS=google; __adroll_fpc=d78c35d38a2a28e116e62524baad1942-1604274039881; _fbp=fb.1.1604274040614.928717410; __qca=P0-455075398-1604274040693; zs=BmuePgtrv7V6XrQZ3sBJsEs4D82lXw; zss=4GplpJuIU1nTQG_2ifYuF-MkD82lXw; biz_owner=eLWrgh6kobjKBjAsi0aVRVA8n5YtwbkeKvPGb26_HD57szJfyUAhJAc10vZcRUYvyU2FdByDjEkldUyapUcCKs3lNhltCugowXDREkSl_vv8iRMpHVzCJkX60JtraXS7dHidzbl-s4o; _gcl_au=1.1.2059619060.1604701459; _scid=15d28b0d-3b00-48f7-a473-ef56d1de83a6; _sctr=1|1604638800000; qntcst=D; bse=0aa2191c45c34e93bcb0996312223baf; _gid=GA1.2.37882618.1607556701; hsfd=0; recentlocations=; location=%7B%22max_longitude%22%3A+-73.981432%2C+%22address3%22%3A+%22%22%2C+%22min_longitude%22%3A+-74.012702%2C+%22neighborhood%22%3A+%22%22%2C+%22address1%22%3A+%22%22%2C+%22place_id%22%3A+%2248253%22%2C+%22min_latitude%22%3A+40.619712%2C+%22county%22%3A+null%2C+%22unformatted%22%3A+%22Brooklyn%2C+NY+11219%22%2C+%22display%22%3A+%22Brooklyn%2C+NY+11219%22%2C+%22borough%22%3A+%22%22%2C+%22polygons%22%3A+null%2C+%22max_latitude%22%3A+40.647101%2C+%22city%22%3A+%22Brooklyn%22%2C+%22isGoogleHood%22%3A+false%2C+%22language%22%3A+null%2C+%22zip%22%3A+%2211219%22%2C+%22parent_id%22%3A+%22LP7cNZ4wcTZL3aW5iBgomQ%22%2C+%22place_key%22%3A+%22NY%3ANew_York%3ABrooklyn%3A%22%2C+%22country%22%3A+%22US%22%2C+%22provenance%22%3A+%22YELP_GEOCODING_ENGINE%22%2C+%22longitude%22%3A+-73.996690506977%2C+%22location_type%22%3A+%22postal_code%22%2C+%22confident%22%3A+null%2C+%22state%22%3A+%22NY%22%2C+%22latitude%22%3A+40.632672326163295%2C+%22usingDefaultZip%22%3A+false%2C+%22address2%22%3A+%22%22%2C+%22accuracy%22%3A+5%7D; sc=772e235ebe; xcj=1|zaNmjnlLaTYsZmefnqJcckfIukoHVBSoKoedV9AdrHA; __ar_v4=7YX6SJQ4RZAMPB6LZ7CHFF%3A20210008%3A2%7CQB5JPFIKRZDSBOZSULG4YB%3A20210008%3A2%7CBHPKS4B4ONEJJMGH4QCJZR%3A20210008%3A2%7CEQVRK2XBL5DFJGDPVSDEYH%3A20201124%3A1; _gat_www=1' \
+  --compressed
