654dc4e8a5c5f71f1432074389eaf92ea04b9464
root <root@Lloyd.localdomain>
2021-01-18 20:15:41 -0500

    [ server.js ] Update server configuration.

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ console.log("__dirname:", __dirname);
 
 app.use(express.static(path.join(__dirname, './build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
