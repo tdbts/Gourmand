9440873ebb30e86b3e7f0aedbd0903ff1f4fbf96
root <root@Lloyd.localdomain>
2021-02-24 06:48:53 -0500

    [ Header, SearchForm, App.css ] Further prettification of components, and preservation of functionality.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,42 @@
+import { useState } from 'react';
+import {
+  Container,
+  Col,
+  Row,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText  // Slogan?
+} from 'reactstrap';
 import SearchForm from './SearchForm';
 
 function Header({onSearchRequest, searching}) {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggle = () => setIsOpen(!isOpen);
+
 	return (
-		<div className="header-container">
-			<div className="header-content-wrapper">
-				<h1 className="title-header">Gourmand</h1>
-				<SearchForm onSearchRequest={onSearchRequest} searching={searching} />
-			</div>
-		</div>
-	);	
+		<Navbar color="light" light expand="md">
+			<NavbarBrand href="/">Gourmand</NavbarBrand>
+        	<NavbarToggler onClick={toggle} />
+        	<Collapse isOpen={isOpen} navbar>
+        		<Nav className="header-nav" navbar>
+					<NavItem>
+						<NavLink href="#">Contact</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="#">Log In</NavLink>
+					</NavItem>										        			
+					<NavItem>
+						<SearchForm onSearchRequest={onSearchRequest} searching={searching} />
+					</NavItem>
+        		</Nav>
+        	</Collapse>
+		</Navbar>
+	);
 }
 
 export default Header;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Container, Row, Col, Nav, Form, InputGroup, InputGroupAddon, Input, Button, Spinner } from 'reactstrap';
+import { Container, Row, Col, Nav, NavItem, Form, InputGroup, InputGroupAddon, Input, Button, Spinner } from 'reactstrap';
 import Suggestions from './Suggestions';
 
 function SearchForm({onSearchRequest, searching}) {
@@ -33,39 +33,25 @@ function SearchForm({onSearchRequest, searching}) {
 
 	return (
 		<form className="search-form" onSubmit={(e) => onSubmit(e, onSearchRequest)}>
-			<Container className="input-container px-0">
-				<Row>
-					<Col className="description-input-column" xs="12" md="4">
-						<Nav className="navbar-light">
-							<div className="input-group-container">
-								<InputGroup>
-									<InputGroupAddon addonType="prepend">
-										<img className="input-icon search-icon" src="magnifying-glass.svg" />
-									</InputGroupAddon>
-									<Input className="search-input" type="text" value={description} onChange={onDescriptionChange} placeholder="e.g. Pizza" />
-								</InputGroup>
-							</div>
-						</Nav>
-					</Col>
-					<Col xs="12" md="8">
-						<Nav className="navbar-light">
-							<div className="input-group-container">
-								<InputGroup>
-									<InputGroupAddon addonType="prepend">
-										<img className="input-icon location-icon" src="target.svg" />
-									</InputGroupAddon>
-									<Input className="search-input" type="text" value={location} onChange={onLocationChange} onClick={toggleSuggestions}
-										onBlur={hideSuggestions} placeholder="e.g. Brooklyn, NY 11237" />
-									<Button className="search-button" color="outline-success" type="submit">
-										{searching ? <Spinner color="dark" size="sm" /> : "Search"}
-									</Button>
-								</InputGroup>
-								{suggestionsOpen && <Suggestions requestLocation={requestLocation} />}
-							</div>
-						</Nav>
-					</Col>
-				</Row>
-			</Container>
+			<div className="input-group-container">
+				<InputGroup>
+					<InputGroupAddon addonType="prepend">
+						<img className="input-icon search-icon" src="magnifying-glass.svg" />
+					</InputGroupAddon>
+					<Input className="search-input" type="text" value={description} onChange={onDescriptionChange} placeholder="e.g. Pizza" />
+				</InputGroup>
+			</div>
+			<div className="input-group-container">
+				<InputGroup>
+					<InputGroupAddon addonType="prepend">
+						<img className="input-icon location-icon" src="target.svg" />
+					</InputGroupAddon>
+					<Input className="search-input" type="text" value={location} onChange={onLocationChange} onClick={toggleSuggestions}
+						onBlur={hideSuggestions} placeholder="e.g. Brooklyn, NY 11237" />
+				</InputGroup>
+				{suggestionsOpen && <Suggestions requestLocation={requestLocation} />}
+			</div>
+			<Button className="search-submit-button" type="submit"></Button>
 		</form>
 	);
 }
