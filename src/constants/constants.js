e09e7428c46a0946895f1ac3c225efc81fa76924
Vinny Sanchez <vinnysanchez87@gmail.com>
2021-03-28 13:14:09 -0400

    [ constants.js ] Prefix environment variable used on frontend with 'REACT_APP_'.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export default {
     EVENT_TRACKING_TOKEN: (process.env.NODE_ENV === 'production')
-        ? process.env.EVENT_TRACKING_TOKEN
+        ? process.env.REACT_APP_EVENT_TRACKING_TOKEN
         : "dadd0494ac9b4ff110cc4a52ba61a65a"
 }
