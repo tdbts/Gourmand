70acb17a37017f4f6b68e5071d97c4c05a9afee4
Vinny Sanchez <vinnysanchez87@gmail.com>
2021-03-28 14:07:25 -0400

    [ constants.js ] Add conditional logic for retrieving non-secret event-tracking tokens.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
+import getEventTrackingToken from "./getEventTrackingToken";
+
 export default {
-    EVENT_TRACKING_TOKEN: (process.env.NODE_ENV === 'production')
-        ? process.env.REACT_APP_EVENT_TRACKING_TOKEN
-        : "dadd0494ac9b4ff110cc4a52ba61a65a"
+    EVENT_TRACKING_TOKEN: getEventTrackingToken(window.location.hostname)
 }
