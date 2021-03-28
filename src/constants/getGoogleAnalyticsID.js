fea934f44193058e4d0ed8e80a9937937cf3babb
Vinny Sanchez <vinnysanchez87@gmail.com>
2021-03-28 17:49:12 -0400

    [ getGoogleAnalyticsID() ] Patch GA setup and update tracking code.

--- src/constants/getGoogleAnalyticsID.js
+++ src/constants/getGoogleAnalyticsID.js
@@ -2,9 +2,9 @@ const getGoogleAnalyticsID = (hostname) => {
     let id = null;
 
     if (hostname.includes("gourmandizer-staging")) {
-        id = "G-424KDS2V7N";
+        id = "UA-193188133-1";
     } else if (hostname.includes("gourmandizer")) {
-        id = "G-GZCWVEGJ47";
+        id = "UA-193112804-1";
     }
 
     return id;
