c26745cb7067bf258f595af68cf05f38681ceb7b
Vinny Sanchez <vinnysanchez87@gmail.com>
2021-05-13 04:38:15 -0400

    [ FeatureNotAvailable ] Add contrast overlay.

--- src/components/App/common/FeatureNotAvailable/FeatureNotAvailable.js
+++ src/components/App/common/FeatureNotAvailable/FeatureNotAvailable.js
@@ -2,8 +2,8 @@ import './FeatureNotAvailable.css';
 import { Container } from 'reactstrap';
 
 const FeatureNotAvailable = ({ featureName }) => (
-    <div className="feature-not-available-container">
-        <div className="feature-not-available-background" style={{backgroundImage: 'url(/feature-not-available-background.jpg)'}} />
+    <div className="feature-not-available-container" style={{backgroundImage: 'url(/feature-not-available-background.jpg)'}}>
+        <div className="contrast-overlay" />
         <Container className="feature-not-available-content">
             <div className="text-container with-image-underlay">
                 <h1 className="content-header">{`We're sorry, but the ${featureName} page isn't yet available.`}</h1>
