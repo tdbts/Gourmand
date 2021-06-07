f197d266fbe8bb62ec1cc847c96149d16d52a3fe
Vinny Sanchez <vinnysanchez87@gmail.com>
2021-06-07 13:57:20 -0400

    [ server.js ] Additional configuration for email transport.

--- src/server/server.js
+++ src/server/server.js
@@ -42,25 +42,21 @@ mongoose
 	.then(() => console.log('MongoDB Connected'))
 	.catch(err => console.log(err));
 
-let transporter;
+const transporter = nodemailer.createTransport({
+	host: "smtp.gmail.com",
+	port: 587,
+	auth: {
+		user: process.env.CONTACT_EMAIL,
+		pass: process.env.CONTACT_EMAIL_PASSWORD
+	},
+	debug: true,
+	logger: true
+});
 
-try {
-	transporter = nodemailer.createTransport({
-		host: "smtp.gmail.com",
-		port: 587,
-		auth: {
-			user: process.env.CONTACT_EMAIL,
-			pass: process.env.CONTACT_EMAIL_PASSWORD
-		}
-	});
-
-	// verify connection configuration
-	transporter.verify()
-		.then(() => console.log("Email server is ready for messages."))
-		.catch((err) => console.log(err));
-} catch (e) {
-	console.error(`Exception on email transport startup: ${e}`);
-}
+// verify connection configuration
+transporter.verify()
+	.then(() => console.log("Email server is ready for messages."))
+	.catch((err) => console.log(err));
 
 app.use(cors());
 app.use(cookieParser('keyboard cat'));
